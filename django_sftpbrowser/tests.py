0daefa9d8973f200397c27a5f26ce7438cfefc22
madtyn <madtyn@gmail.com>
2019-12-28 02:46:37 +0100

    Adapts the tests to the new templates and gets them passed refactoring the browse view

--- django_sftpbrowser/tests.py
+++ django_sftpbrowser/tests.py
@@ -1,27 +1,10 @@
-from django.http import HttpRequest
 from django.test import TestCase
-from django.urls import resolve
-from django_sftpbrowser.views import browse_page
 
-from django.utils import translation
-from django.utils.translation import trans_real
+SERVER_URL = 'https://localhost:8000'
 
 
 # Create your tests here.
 class BrowsePageTest(TestCase):
-    def test_browse_url_resolves_to_browse_page(self):
-        # self.client.get('browse/')
-        lg = trans_real.get_language()
-        lg2 = translation.get_language_from_path('https://localhost:8000/')
-        trans_real.activate(lg)
-        found = resolve('browse/')
-        self.assertEqual(found.func, browse_page)
-
     def test_browse_page_returns_correct_html(self):
-        request = HttpRequest()
-        response = browse_page(request)
-        html = response.content.decode('utf8')
-        self.assertTrue(html.startswith('<html>'))
-        self.assertIn('elcome', html)
-        self.assertTrue(html.endswith('</html>'))
-
+        response = self.client.get(f'{SERVER_URL}/en/browse/')
+        self.assertTemplateUsed(response, 'browse.html')
--- django_sftpbrowser/views.py
+++ django_sftpbrowser/views.py
@@ -1,7 +1,7 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 
 # Create your views here.
 def browse_page(request, input_path=None):
-    return HttpResponse(f'<html>Welcome to <span>{input_path}</span></html>')
+    return render(request, 'browse.html', {'input_path': input_path})
+    # return HttpResponse(f'<html>Welcome to <span>{input_path}</span></html>')
