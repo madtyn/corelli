9438d9816335a2af7b5fde470761930c33035993
madtyn <madtyn@gmail.com>
2019-12-28 18:49:12 +0100

    First iterations for unit tests before retrieving content via SFTP

--- django_sftpbrowser/tests.py
+++ django_sftpbrowser/tests.py
@@ -8,3 +8,12 @@ class BrowsePageTest(TestCase):
     def test_browse_page_returns_correct_html(self):
         response = self.client.get(f'{SERVER_URL}/en/browse/')
         self.assertTemplateUsed(response, 'browse.html')
+
+    def test_browse_page_returns_content_at_root(self):
+        response = self.client.get(f'{SERVER_URL}/en/browse/')
+        self.fail('Not implemented yet. Finish this!')
+
+    def test_browse_page_returns_content_at_folder(self):
+        response = self.client.get(f'{SERVER_URL}/en/browse/upload')
+        self.assertTemplateUsed(response, 'browse.html')
+        self.fail('Not implemented yet. Finish this!')
