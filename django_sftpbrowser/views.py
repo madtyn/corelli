d1ef0d8f190479556f8920f0b0115f32de948e7f
madtyn <madtyn@gmail.com>
2019-12-28 18:50:57 +0100

    Expands the template and view for managing parameters

--- django_sftpbrowser/views.py
+++ django_sftpbrowser/views.py
@@ -3,5 +3,11 @@ from django.shortcuts import render
 
 # Create your views here.
 def browse_page(request, input_path=None):
-    return render(request, 'browse.html', {'input_path': input_path})
+    entries = []
+    return render(request,
+                  'browse.html',
+                  {
+                      'input_path': input_path,
+                      'entries': entries,
+                  })
     # return HttpResponse(f'<html>Welcome to <span>{input_path}</span></html>')
