577dd9753c3a6904be321c9533b9795938123c29
madtyn <madtyn@gmail.com>
2020-01-06 19:07:47 +0100

    Changes config files. SFTP url and default email name

--- config/settings/local.py
+++ config/settings/local.py
@@ -61,3 +61,23 @@ INSTALLED_APPS += ["django_extensions"]  # noqa F405
 
 # Your stuff...
 # ------------------------------------------------------------------------------
+CORELLI_SFTP_SERVER_URL = 'corelli'
+
+"""
+Https stuff
+
+# https://docs.djangoproject.com/en/dev/topics/security/#ssl-https
+# https://docs.djangoproject.com/en/dev/ref/settings/#secure-hsts-seconds
+# TODO: set this to 60 seconds first and then to 518400 once you prove the former works
+SECURE_HSTS_SECONDS = None  # 60
+# https://docs.djangoproject.com/en/dev/ref/settings/#secure-hsts-include-subdomains
+SECURE_HSTS_INCLUDE_SUBDOMAINS = False
+# https://docs.djangoproject.com/en/dev/ref/settings/#secure-proxy-ssl-header
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+# https://docs.djangoproject.com/en/dev/ref/settings/#secure-ssl-redirect
+SECURE_SSL_REDIRECT = False
+# https://docs.djangoproject.com/en/dev/ref/settings/#session-cookie-secure
+SESSION_COOKIE_SECURE = False
+# https://docs.djangoproject.com/en/dev/ref/settings/#csrf-cookie-secure
+CSRF_COOKIE_SECURE = False
+"""
--- config/settings/production.py
+++ config/settings/production.py
@@ -77,13 +77,13 @@ TEMPLATES[0]["OPTIONS"]["loaders"] = [  # noqa F405
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#default-from-email
 DEFAULT_FROM_EMAIL = env(
-    "DJANGO_DEFAULT_FROM_EMAIL", default="corelli <noreply@example.com>"
+    "DJANGO_DEFAULT_FROM_EMAIL", default="Corelli project <noreply@corelli.sytes.net>"
 )
 # https://docs.djangoproject.com/en/dev/ref/settings/#server-email
 SERVER_EMAIL = env("DJANGO_SERVER_EMAIL", default=DEFAULT_FROM_EMAIL)
 # https://docs.djangoproject.com/en/dev/ref/settings/#email-subject-prefix
 EMAIL_SUBJECT_PREFIX = env(
-    "DJANGO_EMAIL_SUBJECT_PREFIX", default="[corelli]"
+    "DJANGO_EMAIL_SUBJECT_PREFIX", default="[Corelli project]"
 )
 
 # ADMIN
@@ -156,3 +156,4 @@ LOGGING = {
 
 # Your stuff...
 # ------------------------------------------------------------------------------
+CORELLI_SFTP_SERVER_URL = 'corelli.sytes.net'
